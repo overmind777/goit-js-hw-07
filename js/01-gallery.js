405c50399fd9872397f63810ddb4aa03f3084b9e
overmind777 <63595243+overmind777@users.noreply.github.com>
2023-09-19 21:19:54 +0300

    create project

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from './gallery-items.js';
+
+const listEl = document.querySelector('.gallery');
+
+function createMurkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    }).join('');
+}
+
+listEl.insertAdjacentHTML('beforeend', createMurkup(galleryItems));
+
+function onPressEsc(e) {
+    if (e.code !== 'Escape') return;
+    instance.close();
+    window.removeEventListener('keydown', onPressEsc);
+}
+
+
+const instance = basicLightbox.create(`<div class="modal"></div>`);
+
+listEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    instance.element().querySelector('.modal').innerHTML = `<img width="1280" height="auto" src="${e.target.dataset.source}">`;
+
+    if (e.target.nodeName !== "IMG") return;
+    
+    window.addEventListener('keydown', onPressEsc);
+    instance.show();
+})
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,33 @@
+import { galleryItems } from './gallery-items.js';
+
+const listEl = document.querySelector('.gallery');
+
+function createMurkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    }).join('');
+}
+
+listEl.insertAdjacentHTML('beforeend', createMurkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
+
+
+
+
+
+
